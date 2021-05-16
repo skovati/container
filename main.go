a166bdb8aaa52910d8774fa63c7e2ed7629e9f62
skovati <skovati@protonmail.com>
2021-05-16 16:25:05 -0500

    add rootless namespace and cgroup implementation

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"os"
 	"os/exec"
     "syscall"
+    "path/filepath"
+    "io/ioutil"
+    "strconv"
 )
 
 func main() {
@@ -12,6 +15,8 @@ func main() {
     switch os.Args[1] {
     case "run":
         run()
+    case "child":
+        child()
     // otherwise, unintended behavior
     default:
         panic("oops")
@@ -22,7 +27,53 @@ func run() {
     // print command info
 	fmt.Printf("Running %v as PID %d\n", os.Args[2:], os.Getpid())
 
-    // setup Command
+    // setup Command to run self exec
+    // this is done in order to create the new namespace and enter it
+    // before we exec the real command
+    cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+
+    // setup pipelines
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+    // clone vals
+    cmd.SysProcAttr = &syscall.SysProcAttr{
+        // create new UTS, PID space, and UID space, as well as new mount space
+        Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWUSER,
+        
+        // set up mapping between UID 0 and current OS UID
+        Credential: &syscall.Credential {
+            Uid: 0,
+            Gid: 0,
+        },
+        UidMappings: []syscall.SysProcIDMap {
+            {
+                ContainerID: 0,
+                HostID: os.Getuid(),
+                Size: 1,
+            },
+        },
+        GidMappings: []syscall.SysProcIDMap {
+            {
+                ContainerID: 0,
+                HostID: os.Getgid(),
+                Size: 1,
+            },
+        },
+    }
+
+    // finally, run the command
+	must(cmd.Run())
+}
+
+func child() {
+    // print command info
+	fmt.Printf("Running %v as PID %d\n", os.Args[2:], os.Getpid())
+
+    cgroup()
+
+    // setup actual command to be runCommand
     cmd := exec.Command(os.Args[2], os.Args[3:]...)
 
     // setup pipelines
@@ -30,13 +81,33 @@ func run() {
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-    // clone new UTS (hostname)
-    cmd.SysProcAttr = &syscall.SysProcAttr {
-        Cloneflags: syscall.CLONE_NEWUTS,
-    }
+    // set hostname within new UTS
+    must(syscall.Sethostname([]byte("container")))
+
+    // chroot and chdir into alpine rootfs
+    must(syscall.Chroot("/home/skovati/code/git/gocon/rootfs"))
+    must(syscall.Chdir("/"))
+
+    //
+    must(syscall.Mount("proc", "proc", "proc", 0, ""))
 
     // finally, run the command
 	must(cmd.Run())
+
+    // and unmount proc
+    must(syscall.Unmount("proc", 0))
+}
+
+func cgroup() {
+    cgroups := "/sys/fs/cgroup/"
+    pids := filepath.Join(cgroups, "pids")
+
+    must(os.Mkdir(filepath.Join(pids, "alp"), 0755))
+    must(ioutil.WriteFile(filepath.Join(pids, "alp/pids.max"), []byte("20"), 0700))
+
+    // removes cgroup in place after the container exits
+    must(ioutil.WriteFile(filepath.Join(pids, "alp/notify_on_release"), []byte("1"), 0700))
+    must(ioutil.WriteFile(filepath.Join(pids, "alp/cgroup.procs"), []byte(strconv.Itoa(os.Getpid())), 0700))
 }
 
 func must(err error) {
