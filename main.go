27c5cafb77b293670b71d9032abcb6168cb8cab2
skovati <skovati@protonmail.com>
2021-05-16 12:53:30 -0500

    add alpine rootfs

--- main.go
+++ main.go
@@ -1,28 +1,46 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "os/exec"
+	"fmt"
+	"os"
+	"os/exec"
+    "syscall"
 )
 
 func main() {
-
+    // switch on first argument, commonly "run"
+    switch os.Args[1] {
+    case "run":
+        run()
+    // otherwise, unintended behavior
+    default:
+        panic("oops")
+    }
 }
 
 func run() {
-    fmt.Printf("Running %v as PID %d\n", os.Args[2:], os.Getpid())
+    // print command info
+	fmt.Printf("Running %v as PID %d\n", os.Args[2:], os.Getpid())
 
-    cmd := exec.Command()
-    cmd.Stdin = os.Stdin
-    cmd.Stdout = os.Stdout
-    cmd.Stderr = os.Stderr
+    // setup Command
+    cmd := exec.Command(os.Args[2], os.Args[3:]...)
 
-    must(cmd.Run())
+    // setup pipelines
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+    // clone new UTS (hostname)
+    cmd.SysProcAttr = &syscall.SysProcAttr {
+        Cloneflags: syscall.CLONE_NEWUTS,
+    }
+
+    // finally, run the command
+	must(cmd.Run())
 }
 
 func must(err error) {
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 }
